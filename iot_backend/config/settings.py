ec33d496a92daf93f24e58b74b7282ada2013ef7
monikashah <monikapatira@gmail.com>
2026-04-10 15:03:52 +0530

    Updated model

--- iot_backend/config/settings.py
+++ iot_backend/config/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ingestion',
+    'rest_framework',
+    'corsheaders',
+    'django.contrib.gis',
+    'django_extensions',
+    
 ]
 
 MIDDLEWARE = [
