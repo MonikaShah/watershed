12a999c961087cb50333fee642a0cea54fd74548
monikashah <monikapatira@gmail.com>
2026-04-10 14:58:16 +0530

    Updated model

--- iot_backend/ingestion/models.py
+++ iot_backend/ingestion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Device(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=255)
+    label = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'device'
