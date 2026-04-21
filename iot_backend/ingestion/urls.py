98125e2547ef8da1acc70c153a2a72f2b60f929e
monikashah <monikapatira@gmail.com>
2026-04-21 16:21:00 +0530

    Urls changes

--- iot_backend/ingestion/urls.py
+++ iot_backend/ingestion/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('ingest/', views.ingest_data, name='ingest_data'),
     path('ota/<str:device_type>/', views.ota, name='ota'),
-    path('tb-dashboard/', views.dashboard, name='dashboard'),
-    path('tb-dashboard/export-csv/', views.export_csv, name='export_csv'),
+    path('iot-dashboard/', views.dashboard, name='dashboard'),
+    path('iot-dashboard/export-csv/', views.export_csv, name='export_csv'),
     # path('db/', views.db_tables, name='db_tables'),
     # path('db/<str:table_name>/', views.db_table_view, name='db_table_view'),
 
