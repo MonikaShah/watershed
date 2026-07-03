e0a3a183f50c4556d4f08c040d22723c7f881cb2
monikashah <monikapatira@gmail.com>
2026-07-03 16:41:24 +0530

    settings updated

--- iot_backend/config/settings.py
+++ iot_backend/config/settings.py
@@ -100,16 +100,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    
+    },
+    'thingsboard': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
-    # },
-    # 'thingsboard': {
-    #     'ENGINE': config('DB_ENGINE'),
-    #     'NAME': config('DB_NAME'),
-    #     'USER': config('DB_USER'),
-    #     'PASSWORD': config('DB_PASSWORD'),
-    #     'HOST': config('DB_HOST'),
-    #     'PORT': config('DB_PORT'),
-    # }
 }
 
 # DATABASES = {
