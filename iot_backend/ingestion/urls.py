65062ca06f3adc7428083b69faef53c12ffc6e29
monikashah <monikapatira@gmail.com>
2026-07-08 18:01:20 +0530

    urls updated

--- iot_backend/ingestion/urls.py
+++ iot_backend/ingestion/urls.py
@@ -11,20 +11,20 @@ urlpatterns = [
     path('iot-dashboard/export-csv/', views.export_csv, name='export_csv'),
     # path('db/', views.db_tables, name='db_tables'),
     # path('db/<str:table_name>/', views.db_table_view, name='db_table_view'),
-    path(
-        'iot-dashboard/compare/',
-        views.dashboard_compare,
-        name='dashboard_compare'
-    ),
+    # path(
+    #     'iot-dashboard/compare/',
+    #     views.dashboard_compare,
+    #     name='dashboard_compare'
+    # ),
     # path( "device-status/", views.device_status, name="device_status",),
     path("device-logs/", views.device_logs, name="device_logs"),
     path("device-logs-ui/", views.device_logs_page, name="device_logs_page"),
     path("device-status-ui/", views.device_status_dashboard, name="device_status_dashboard"),
     # NEW API
-    path(
-        'api/device-comparison/',
-        views.device_comparison_api,
-        name='device_comparison_api'
-    ),
+    # path(
+    #     'api/device-comparison/',
+    #     views.device_comparison_api,
+    #     name='device_comparison_api'
+    # ),
     
 ]
